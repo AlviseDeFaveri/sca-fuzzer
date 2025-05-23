9453f700c6c7bf9f3ab6cbfd7c64b838abf455f7
Alvise <26742412+AlviseDeFaveri@users.noreply.github.com>
2025-05-23 11:06:53 +0000

    [tmp] Add trace parser
    - Add separate tools folder
    - Add parser for the debug logs/traces

--- rvzr/model_dynamorio/backend/tracer_abc.cpp
+++ rvzr/model_dynamorio/backend/tracer_abc.cpp
@@ -4,16 +4,11 @@
 // Copyright (C) Microsoft Corporation
 // SPDX-License-Identifier: MIT
 
-#include <cstddef>
 #include <cstdint>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#include <new>
-#include <optional>
 #include <string>
-#include <utility>
-#include <vector>
 
 #include <dr_api.h> // NOLINT
 #include <dr_defines.h>
@@ -29,71 +24,8 @@
 
 #include "observables.hpp"
 #include "tracer_abc.hpp"
-#include "util.hpp"
 
 using std::string;
-
-// =================================================================================================
-// Local helper functions for allocating and freeing memory
-// =================================================================================================
-
-/// @brief Print function for the trace type
-static constexpr const char *to_string(const trace_entry_type_t &type)
-{
-    switch (type) {
-    case trace_entry_type_t::ENTRY_EOT:
-        return "EOT";
-    case trace_entry_type_t::ENTRY_PC:
-        return "PC";
-    case trace_entry_type_t::ENTRY_READ:
-        return "READ";
-    case trace_entry_type_t::ENTRY_WRITE:
-        return "WRITE";
-    case trace_entry_type_t::ENTRY_REG_DUMP_ARCH:
-        return "ARCH";
-    case trace_entry_type_t::ENTRY_REG_DUMP_SPEC:
-        return "SPEC";
-    default:
-        return "UNKNOWN";
-    }
-}
-
-/// @brief Print function for normal traces
-static void dump(const trace_entry_t &entry, const file_t &where)
-{
-    dr_fprintf(where, "[%s] addr: %lx  (sz: %u)\n", to_string(entry.type), entry.addr, entry.size);
-}
-
-/// @brief Print function for debug traces
-static void dump(const dbg_trace_entry_t &entry, const file_t &where)
-{
-    using type = trace_entry_type_t;
-    if (entry.type == trace_entry_type_t::ENTRY_READ or
-        entry.type == trace_entry_type_t::ENTRY_WRITE) {
-        dr_fprintf(where, "[%s] addr: %lx  val: %lx (sz: %u)\n", to_string(entry.type),
-                   entry.mem.address, entry.mem.value, entry.mem.size);
-    } else {
-        dr_fprintf(
-            where,
-            "[%s] pc: %lx  (rax: 0x%lx rbx: 0x%lx rcx: 0x%lx rdx: 0x%lx rsi: 0x%lx rdi: 0x%lx)\n",
-            to_string(entry.type), entry.regs.pc, entry.regs.xax, entry.regs.xbx, entry.regs.xcx,
-            entry.regs.xdx, entry.regs.xsi, entry.regs.xdi);
-    }
-}
-
-/// @brief Parse traces of type T from a file and dump them to the provided output
-/// @tparam T File is expected to contain traces of this typ
-/// @param filename File to read traces from
-/// @param where Where to print the parsed entries
-template <typename T> static void dump_from_file(const std::string &filename, const file_t &where)
-{
-    std::fstream file(filename, std::ios::in | std::ios::binary);
-    T entry{};
-    while (not file.eof()) {
-        file.read((char *)&entry, sizeof(T));
-        dump(entry, where);
-    }
-}
 
 // =================================================================================================
 // Constructors and Destructors
