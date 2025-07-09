6026544f7269ecbd42c66641e5464462d5a0bace
Alvise <26742412+AlviseDeFaveri@users.noreply.github.com>
2025-07-09 14:03:02 +0000

    [tmp] fix plugin

--- consfuzz/inspector/plugin.py
+++ consfuzz/inspector/plugin.py
@@ -11,8 +11,9 @@ import gdb
 import sys
 sys.path.append("/home/alvise/venv-revizor-2/lib/python3.12/site-packages")
 sys.path.append(".")
+sys.path.append("..")
 
-from typing import Any, Optional
+from typing import Any, List, Optional
 from rvzr.model_dynamorio.trace_decoder import TraceDecoder, DebugTraceEntryType
 
 # ------------------------------------------------------------------------------
@@ -21,8 +22,10 @@ from rvzr.model_dynamorio.trace_decoder import TraceDecoder, DebugTraceEntryType
 
 _glob_trace = None
 _cached_lines = {}
+
 _glob_cur_line = None
-_glob_cur_level = 0
+_glob_cur_spec_level = None
+_glob_cur_spec_context = None
 
 class SpecWinInfo:
     """
@@ -48,9 +51,14 @@ class SpecWinInfo:
         if self.first_pc is None:
             return f"{prefix} └─ last architectural: {hex(self.target_pc)} (#{self.target_count}) [line: {self.target_line}]"
 
-        s  = f"{prefix} ├─ speculates to: {hex(self.first_pc)} [line: {self.first_line}]\n"
-        s += f"{prefix} └─ until: {hex(self.target_pc)} (#{self.target_count}) [line: {self.target_line}]"
-        return s
+        # Check if we're at the beginning of this window
+        global _glob_cur_line
+        if _glob_cur_line == self.first_line:
+            return f"{prefix} └─ last: {hex(self.first_pc)} [line: {self.first_line}]\n"
+        else:
+            s = f"{prefix}  ├─ speculated to: {hex(self.first_pc)} [line: {self.first_line}]\n"
+            s += f"{prefix} └─ last: {hex(self.target_pc)} (#{self.target_count}) [line: {self.target_line}]"
+            return s
 
 
 def _is_arch(entry: Any) -> bool:
@@ -120,8 +128,6 @@ def _get_line_info(line: int):
 # GDB Helpers
 # ------------------------------------------------------------------------------
 
-_glob_n_bp = 0
-
 class Printing:
     """
     Customize the amount of information printed when running commands
@@ -134,23 +140,28 @@ class Printing:
     def restore():
         gdb.execute("set print frame-arguments all")
 
-class Breakpoints:
+class BpManager:
     """
     Breakpoints management
     """
     @staticmethod
     def add(bp: str | int) -> int:
-        global _glob_n_bp
         if isinstance(bp, int):
-            gdb.execute(f"b *{hex(bp)}", to_string=True)
+            result = gdb.execute(f"b *{hex(bp)}", to_string=True)
         else:
-            gdb.execute(f"b {bp}", to_string=True)
+            result = gdb.execute(f"b {bp}", to_string=True)
 
-        _glob_n_bp += 1
-        return _glob_n_bp
+        if result.startswith("Breakpoint"):
+            bp_num = int(result.split(" ")[1])
+        else:
+            print(result)
+            raise ValueError("Cannot add breakpoint")
+
+        return bp_num
 
     @staticmethod
     def delete(n: int):
+        gdb.execute(f"dis {n}")
         gdb.execute(f"del {n}")
 
     @staticmethod
@@ -210,47 +221,50 @@ class SpecSourceCommand (gdb.Command):
         global _glob_trace
         _glob_trace = dbg_traces[0]
 
-        # Run the program
-        bp = Breakpoints.add("wrapper")
-        GdbExec.run()
-        Breakpoints.delete(bp)
-
         print(" • Done!")
 
 SpecSourceCommand ()
 
 
-def spec_goto():
-    # Get spec info for the given line
-    global _glob_cur_line
-    global _glob_cur_level
-    spec_windows = _get_line_info(_glob_cur_line)
-
+def spec_goto(context: List[SpecWinInfo], depth: int = 0, stop_at_first: bool = False):
     Printing.setup()
+
+    # Restart the program
+    bp = BpManager.add("wrapper")
+    GdbExec.run()
+    BpManager.delete(bp)
+
     # Simulate all windows
-    for win in reversed(spec_windows[_glob_cur_level:]):
+    relevant_context = context[depth:]
+    for cur_n_win, win in enumerate(reversed(relevant_context)):
         # Check if we need to open a speculation window
         if win.first_pc is not None:
             # Break on the first speculative instruction
-            bp = Breakpoints.add(win.first_pc)
+            bp = BpManager.add(win.first_pc)
             # Jump to the first speculative instruction (stops at previous breakpoint)
             GdbExec.jump(win.first_pc)
             # Disable entrypoint (in case it's visited again in the rest of the trace)
-            Breakpoints.delete(bp)
+            BpManager.delete(bp)
 
-        # Break on last instruction of the window
-        bp = Breakpoints.add(win.target_pc)
-        # Continue until the right number of occurrences have been found
-        Breakpoints.ignore(bp, win.target_count - 1)
-        GdbExec.cont()
-        # Disable breakpoint (in case we encounter this instruction again later in the trace)
-        Breakpoints.delete(bp)
+        # Check if we need to stop at the first instruction of the current window
+        if stop_at_first:
+            if cur_n_win == len(relevant_context) -1:
+                break
+
+        # Check if this window has only one instruction
+        if win.first_line != win.target_line:
+            # Break on last instruction of the window
+            bp = BpManager.add(win.target_pc)
+            # Continue until the right number of occurrences have been found
+            BpManager.ignore(bp, win.target_count - 1)
+            GdbExec.cont()
+            # Disable breakpoint (in case we encounter this instruction again later in the trace)
+            BpManager.delete(bp)
         # Print Backtrace
         # print(f"------------------- Reached {hex(win.target_pc)} ----------------------")
         # GdbExec.backtrace()
 
     Printing.restore()
-    gdb.execute("dash")
 
 
 class SpecGotoCommand (gdb.Command):
@@ -261,8 +275,6 @@ class SpecGotoCommand (gdb.Command):
         super (SpecGotoCommand, self).__init__ ("spec goto",
                                                        gdb.COMMAND_SUPPORT,
                                                        gdb.COMPLETE_FILENAME)
-        global _glob_n_bp
-        _glob_n_bp = 0
 
     def invoke (self, arg, from_tty):
         # Check validity of state and inputs
@@ -280,8 +292,12 @@ class SpecGotoCommand (gdb.Command):
 
         global _glob_cur_line
         _glob_cur_line = arg
+        global _glob_cur_spec_context
+        _glob_cur_spec_context = _get_line_info(arg)
+        global _glob_cur_spec_level
+        _glob_cur_spec_level = 0
 
-        spec_goto()
+        spec_goto(_glob_cur_spec_context)
 
 SpecGotoCommand ()
 
@@ -296,18 +312,16 @@ class SpecBtCommand (gdb.Command):
                                                        gdb.COMPLETE_FILENAME)
 
     def invoke (self, arg, from_tty):
+        global _glob_cur_spec_context
+        global _glob_cur_spec_level
+
         # Check validity of state and inputs
-        global _glob_cur_line
-        global _glob_cur_level
-        if _glob_cur_line is None:
+        if _glob_cur_spec_context is None or _glob_cur_spec_level is None:
             print ("Error: no line selected - use 'spec goto' to select one")
             return
 
-        # Get spec info for the given line
-        spec_windows = _get_line_info(_glob_cur_line)
-
         # Print spec info
-        for win in reversed(spec_windows[_glob_cur_level:]):
+        for win in reversed(_glob_cur_spec_context[_glob_cur_spec_level:]):
             print(win)
 
 SpecBtCommand ()
@@ -324,18 +338,37 @@ class SpecUpCommand (gdb.Command):
 
     def invoke (self, arg, from_tty):
         global _glob_cur_line
-        global _glob_cur_level
+        global _glob_cur_spec_context
+        global _glob_cur_spec_level
 
         # Check validity of state and inputs
-        if _glob_cur_line is None:
+        if _glob_cur_line is None or _glob_cur_spec_context is None or _glob_cur_spec_level is None:
             print ("Error: no line selected - use 'spec goto' to select one")
             return
-        if _glob_cur_level >= len(_cached_lines[_glob_cur_line]) - 1:
-            print ("Already at top level")
-            return
 
-        _glob_cur_level += 1
-        spec_goto()
+        # Check at what point of the window we are
+        cur_win = _glob_cur_spec_context[_glob_cur_spec_level]
+        is_win_start = (_glob_cur_line == cur_win.first_line)
+
+        if is_win_start:
+            # Goto end of previous window
+            if _glob_cur_spec_level >= len(_glob_cur_spec_context) - 1:
+                print ("Already at top level")
+                return
+
+            _glob_cur_spec_level += 1
+            _glob_cur_line = _glob_cur_spec_context[_glob_cur_spec_level].target_line
+            spec_goto(_glob_cur_spec_context, _glob_cur_spec_level)
+        else:
+            # Goto start of the current window
+            cur_line = _glob_cur_spec_context[_glob_cur_spec_level].first_line
+            if cur_line == None:
+                print ("Hit start of speculation!")
+                return
+
+            _glob_cur_line = cur_line
+            spec_goto(_glob_cur_spec_context, _glob_cur_spec_level, stop_at_first=True)
+
 
 SpecUpCommand()
 
@@ -351,18 +384,32 @@ class SpecDownCommand (gdb.Command):
 
     def invoke (self, arg, from_tty):
         global _glob_cur_line
-        global _glob_cur_level
+        global _glob_cur_spec_context
+        global _glob_cur_spec_level
 
         # Check validity of state and inputs
-        if _glob_cur_line is None:
+        if _glob_cur_line is None or _glob_cur_spec_context is None or _glob_cur_spec_level is None:
             print ("Error: no line selected - use 'spec goto' to select one")
             return
-        if _glob_cur_level == 0:
-            print ("Already at bottom level")
-            return
 
-        _glob_cur_level -= 1
-        spec_goto()
+        # Check at what point of the window we are
+        cur_win = _glob_cur_spec_context[_glob_cur_spec_level]
+        is_win_end = (_glob_cur_line == cur_win.target_line)
+
+        if is_win_end:
+            # Goto start of next window
+            if _glob_cur_spec_level == 0:
+                print ("Already at bottom level")
+                return
+
+            _glob_cur_spec_level -= 1
+            _glob_cur_line = _glob_cur_spec_context[_glob_cur_spec_level].first_line
+            spec_goto(_glob_cur_spec_context, _glob_cur_spec_level, stop_at_first=True)
+        else:
+            # Goto end of the current window
+            _glob_cur_line = _glob_cur_spec_context[_glob_cur_spec_level].target_line
+            spec_goto(_glob_cur_spec_context, _glob_cur_spec_level)
+
 
 SpecDownCommand()
 
@@ -378,19 +425,29 @@ class SpecPrevCommand (gdb.Command):
 
     def invoke (self, arg, from_tty):
         global _glob_cur_line
-        global _glob_cur_level
-        global _glob_trace
+        global _glob_cur_spec_context
+        global _glob_cur_spec_level
 
         while True:
             if _glob_cur_line == 0:
-                print("Already at first instruction!")
+                print("Already at first instruction of the trace!")
                 return
+            if _glob_cur_line == _glob_cur_spec_context[_glob_cur_spec_level].first_line:
+                print("Already at first instruction of the current window!")
+                print("Use 'spec up' to go to the previous window")
+                return
+            # Get cur nesting level
+            cur_nesting = _glob_trace[_glob_cur_line].nesting_level
             # Get previous instruction
             _glob_cur_line -= 1
             entry = _glob_trace[_glob_cur_line]
             if DebugTraceEntryType(entry.type) == DebugTraceEntryType.ENTRY_REG_DUMP:
+                if entry.nesting_level > cur_nesting:
+                    # Ignore unrelated windows
+                    continue
                 # Found new instruction
-                spec_goto()
+                tmp_context = _get_line_info(_glob_cur_line)
+                spec_goto(tmp_context)
                 break
 
 SpecPrevCommand()
@@ -407,19 +464,29 @@ class SpecNextCommand (gdb.Command):
 
     def invoke (self, arg, from_tty):
         global _glob_cur_line
-        global _glob_cur_level
-        global _glob_trace
+        global _glob_cur_spec_context
+        global _glob_cur_spec_level
 
         while True:
             if _glob_cur_line == len(_glob_trace) - 1:
-                print("Already at last instruction!")
+                print("Already at last instruction of the trace!")
                 return
-            # Get previous instruction
+            if _glob_cur_line == _glob_cur_spec_context[_glob_cur_spec_level].target_line:
+                print("Already at last instruction of the current window!")
+                print("Use 'spec down' to enter to the next window")
+                return
+            # Get cur nesting level
+            cur_nesting = _glob_trace[_glob_cur_line].nesting_level
+            # Get next instruction
             _glob_cur_line += 1
             entry = _glob_trace[_glob_cur_line]
             if DebugTraceEntryType(entry.type) == DebugTraceEntryType.ENTRY_REG_DUMP:
+                if entry.nesting_level > cur_nesting:
+                    # Ignore unrelated windows
+                    continue
                 # Found new instruction
-                spec_goto()
+                tmp_context = _get_line_info(_glob_cur_line)
+                spec_goto(tmp_context)
                 break
 
 SpecNextCommand()
